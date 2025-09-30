d011f4016ec6806e30c9dd5b559803d3e0434f7d
Eigintas Urbanavičius <eigintas.urbanavicius@mif.stud.vu.lt>
2025-09-30 21:50:12 +0300

    Added ADT of Card Game Domain

--- README.md
+++ README.md
@@ -7,9 +7,33 @@ Can be viewed [here](https://vipo.github.io/fp-2025/)
 ## Setup
 
 ### To get started, you first need to open the project using Visual Studio Code and having Docker Desktop
+
 1. `Ctrl + Shift + P`
 2. `Dev Containers: Open Folder in Container`
 
 ### To Build & Test the Project, run the following commands
+
 1. `stack build`
 2. `stack test`
+
+## Lib1.hs
+
+### BNF
+
+```
+<Command> ::= "CreateDeck"
+            | "ShuffleDeck"
+            | "DrawCards" <Int>
+            | "AddCard" <Card>
+            | "RemoveCard" <Card>
+            | "ShowDeck"
+            | "CountCards"
+            | "PlayCard" <Card>
+            | "Sequence" <Command> <Command>
+            | "Dump" <Dumpable>
+
+<Card> ::= <Suit> <Rank>
+<Rank> ::= "Ace" | "Two" | "Three" | "Four" | "Five" | "Six" | "Seven" | "Eight" | "Nine" | "Ten" | "Jack" | "Queen" | "King"
+<Suit> ::= "Hearts" | "Clubs" | "Spades" | "Diamonds"
+<Dumpable> ::= "Examples"
+```
--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,15 +2,87 @@ module Lib1
     ( examples, Command(..), Dumpable(..)
     ) where
 
+
+data Card = Card
+  { suit :: Suit
+  , rank :: Rank
+  } deriving Show
+
+
+data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King
+  deriving (Show, Eq, Ord)
+
+data Suit = Hearts | Clubs | Spades | Diamonds
+  deriving (Show, Eq, Ord)
+
+data Deck = Deck 
+    {
+    deckCards :: [Card]
+    } deriving Show
+
+data Hand = Hand
+    {
+    handCards :: [Card] 
+    } deriving Show
+
+data Command
+    = CreateDeck
+    | ShuffleDeck
+    | DrawCards Int
+    | AddCard Card
+    | RemoveCard Card
+    | ShowDeck
+    | CountCards
+    | PlayCard Card
+    | Sequence Command Command
+    | Dump Dumpable
+    deriving Show
+
+
 data Dumpable = Examples
   deriving Show
 
--- This is a "root" ADT representing your grammar,
--- Please expand this ADT as needed
-data Command = Dump Dumpable
-  deriving Show
+aceOfSpades :: Card
+aceOfSpades = Card { suit = Spades, rank = Ace }
+
+kingOfHearts :: Card  
+kingOfHearts = Card { suit = Hearts, rank = King }
+
+queenOfDiamonds :: Card
+queenOfDiamonds = Card { suit = Diamonds, rank = Queen }
+
+myDeck :: Deck
+myDeck = Deck { deckCards = [aceOfSpades, kingOfHearts, queenOfDiamonds]}
+
+fullHouseDeck :: Deck
+fullHouseDeck = Deck
+  { deckCards =
+      [ Card { suit = Spades, rank = Ace }  
+      , Card { suit = Hearts, rank = Ace }
+      , Card { suit = Diamonds, rank = Ace }
+      , Card { suit = Clubs, rank = King }
+      , Card { suit = Spades, rank = King }
+      ]
+  }
+
+example1 :: Command
+example1 = CreateDeck
+
+example2 :: Command  
+example2 = ShuffleDeck
+
+example3 :: Command
+example3 = DrawCards 5
+
+example4 :: Command
+example4 = Sequence CreateDeck (Sequence ShuffleDeck (DrawCards 3))
+
 
 examples :: [Command]
 examples = [
+    example1,
+    example2,
+    example3,
+    example4,
     Dump Examples
     ]
